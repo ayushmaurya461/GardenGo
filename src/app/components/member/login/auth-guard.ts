1e91a0b0b77df5deb00989ece485c0da9c993dbe
Ayush Maurya <ayushmaurya461@gmail.com>
2023-01-11 02:20:27 +0530

    fix

--- src/app/components/member/login/auth-guard.ts
+++ src/app/components/member/login/auth-guard.ts
@@ -1,6 +1,6 @@
 import { Injectable } from "@angular/core";
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from "@angular/router";
-import { map, Observable } from "rxjs";
+import { map, Observable, take } from "rxjs";
 import { LoginService } from "./login.service";
 
 @Injectable({ providedIn: 'root' })
@@ -15,13 +15,15 @@ export class AuthGuard implements CanActivate{
         route: ActivatedRouteSnapshot, 
         state: RouterStateSnapshot
         ): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean>{
-          return this.loginService.user.pipe(map(
+          return this.loginService.user.pipe(
+            take(1),
+            map(
             user => {
               const isAuthenticated = !!user;
               if(isAuthenticated){
                 return true;
               }
-              alert("You are not allowed to view this page.");
+              alert("You are not allowed to view this page. Please login to submit a review.");
               return this.router.createUrlTree(['/login']);
             }
           )
